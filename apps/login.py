aacf1dc99e7ebe2bc9e7719f800e38d2082ec3cc
markjsedan <sedanmarkjay11@gmail.com>
2023-01-10 03:49:47 +0800

    3:49

--- apps/login.py
+++ apps/login.py
@@ -10,7 +10,7 @@ from apps import dbconnect as db
 
 layout = html.Div(
     [
-        html.H2('Please Login'),
+        html.H2('Please Log in'),
         html.Hr(),
         dbc.Alert('Username or password is incorrect.', color="danger", id='login_alert',
                   is_open=False),
