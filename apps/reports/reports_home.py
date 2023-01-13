13c96e9d725dff8d74a73e3b7e417f9cc75669d0
markjsedan <sedanmarkjay11@gmail.com>
2023-01-13 22:07:45 +0800

    10:07

--- apps/reports/reports_home.py
+++ apps/reports/reports_home.py
@@ -19,7 +19,11 @@ layout = html.Div(
                 dbc.CardHeader(html.H4("Reports")),
                 dbc.CardBody(
                     [
-                        html.Button("Download Excel", id="btn_xlsx"),
+                        dbc.Row(
+                            dbc.Col("Top 10 Customers"),
+                            dbc.Button("Download Excel", id="btn_xlsx"),
+                            
+                        ),
                         dcc.Download(id="download-dataframe-xlsx"),
                     ]
                 ),
@@ -27,7 +31,6 @@ layout = html.Div(
         ),
     ],
 )
-df = pd.DataFrame({"a": [1, 2, 3, 4], "b": [2, 1, 5, 6], "c": ["x", "x", "y", "y"]})
 
 @app.callback(
     Output("download-dataframe-xlsx", "data"),
@@ -35,4 +38,20 @@ df = pd.DataFrame({"a": [1, 2, 3, 4], "b": [2, 1, 5, 6], "c": ["x", "x", "y", "y
     prevent_initial_call=True,
 )
 def func(n_clicks):
-    return dcc.send_data_frame(df.to_excel, "mydf.xlsx", sheet_name="Sheet_name_1")
+    # df = pd.DataFrame({"a": [1, 2, 3, 4], "b": [2, 1, 5, 6], "c": ["x", "x", "y", "y"]})
+    
+    sql = """ SELECT cust_ind_name, COUNT(pur_ind_amt), SUM(pur_ind_amt)
+    FROM purchases_individuals
+    INNER JOIN customers_individuals on purchases_individuals.cust_ind_id = customers_individuals.cust_ind_id
+    WHERE NOT pur_ind_delete_ind
+    GROUP BY cust_ind_name
+    ORDER BY SUM(pur_ind_amt) DESC
+    FETCH FIRST 10 ROWS ONLY
+    """
+    val = []
+    cols = ["Customer", "Number of Purchases","Total Amount"]
+        
+    top10_cust = db.querydatafromdatabase(sql,val,cols)
+
+
+    return dcc.send_data_frame(top10_cust.to_excel, "Top 10 Customers.xlsx", sheet_name="Top 10 Customers")
