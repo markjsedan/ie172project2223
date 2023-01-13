0bc1636a09c3e0f9197651680c47784aa4ee8ccc
markjsedan <sedanmarkjay11@gmail.com>
2023-01-13 20:36:40 +0800

    8:37

--- apps/books/genres/genres.py
+++ apps/books/genres/genres.py
@@ -15,6 +15,7 @@ sort_add = dbc.NavbarSimple(
     children=[
         dbc.DropdownMenu(
             children=[
+                dbc.DropdownMenuItem("No filter", href="/books/genres"),
                 dbc.DropdownMenuItem("A-Z", href="/books/genres_atoz"),
                 dbc.DropdownMenuItem("Z-A", href="/books/genres_ztoa"),
             ],
